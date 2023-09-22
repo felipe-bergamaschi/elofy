c69d3c190479fe9e4b66be89386bce32d0a9e6df
Felipe Bergamaschi <midisoft.corporation@gmail.com>
2023-09-22 18:46:49 -0300

    fix: remove jupiter

--- jupiter/frontend/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import Routes from 'vite-plugin-router'
-
-export default defineConfig({
-  plugins: [react(), Routes()],
-})
